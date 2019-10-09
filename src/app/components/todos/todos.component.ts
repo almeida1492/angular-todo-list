beee01d9176a3f62c54996e524ea0586fd8d8068
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-10-09 17:19:01 +0200

    embed TodosComponent in AppComponent

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todos',
+  selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
