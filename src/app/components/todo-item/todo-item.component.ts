bf1c8f688f43d18f2754ef28840950aef8734a9f
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-10-10 13:49:36 +0200

    add styles to TodoItem

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
-  selector: 'todo-item',
-  templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+	selector: 'todo-item',
+	templateUrl: './todo-item.component.html',
+	styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-  @Input() todo:Todo;
+	@Input() todo: Todo;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	//set dynamic classes
+	setClasses() {
+		let classes = {
+			todo: true,
+			'is-complete': this.todo.completed,
+		}
+		return classes;
+	}
 }
